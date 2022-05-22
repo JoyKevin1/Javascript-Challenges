560a4ac91b129cf40053a3223eb5fb212fef4837
FullStackKevin <joykevinrobin1@gmail.com>
2022-05-22 12:09:26 +0200

    CHANGED

---
+++ script.js
@@ -0,0 +1,34 @@
+const selectionButtons = document.querySelectorAll(`[data-selection]`)
+
+const SELECTIONS = [
+    {
+        name: 'rock',
+        emoji: '✊',
+        beats: 'scissors'
+    },
+
+    {
+        name: 'paper',
+        emoji: '✋',
+        beats: 'rock'
+    },
+
+    {
+        name: 'scissors',
+        emoji: '✌',
+        beats: 'paper'
+    }
+]
+
+selectionButtons.forEach(selectionButton => {
+    selectionButton.addEventListener('click', e => {
+const selectionName = selectionButton.dataset.selection
+SELECTIONS.find(selection => selection.name === selectionName)
+makeSelection(selectionName)
+    })
+})
+
+
+function makeSelection(selection){
+    console.log(selection)
+}
